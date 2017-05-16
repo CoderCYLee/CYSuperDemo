5b224ee03b1f6214d420d79ed8a236af9818001d
Cyrill <lichunyang@outlook.com>
2017-05-16 14:00:21 +0800

    cc

--- CYSuperDemo/CYSuperDemo/Utility/CYFramework/Category/CYExtension.h
+++ CYSuperDemo/CYSuperDemo/Utility/CYFramework/Category/CYExtension.h
@@ -124,4 +124,45 @@
 
 #define kDEVICE_5_DIFFERENCE (88.0f)
 
+// 这里是弱引用强引用
+#ifndef    weakify
+#if __has_feature(objc_arc)
+
+#define weakify( x ) \\
+_Pragma("clang diagnostic push") \\
+_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
+autoreleasepool{} __weak __typeof__(x) __weak_##x##__ = x; \\
+_Pragma("clang diagnostic pop")
+
+#else
+
+#define weakify( x ) \\
+_Pragma("clang diagnostic push") \\
+_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
+autoreleasepool{} __block __typeof__(x) __block_##x##__ = x; \\
+_Pragma("clang diagnostic pop")
+
+#endif
+#endif
+
+#ifndef    strongify
+#if __has_feature(objc_arc)
+
+#define strongify( x ) \\
+_Pragma("clang diagnostic push") \\
+_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
+try{} @finally{} __typeof__(x) x = __weak_##x##__; \\
+_Pragma("clang diagnostic pop")
+
+#else
+
+#define strongify( x ) \\
+_Pragma("clang diagnostic push") \\
+_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
+try{} @finally{} __typeof__(x) x = __block_##x##__; \\
+_Pragma("clang diagnostic pop")
+
+#endif
+#endif
+
 #endif
