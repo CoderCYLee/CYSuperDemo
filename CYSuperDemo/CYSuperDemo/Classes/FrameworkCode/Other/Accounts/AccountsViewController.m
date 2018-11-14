5a427712c6d48ede28e309e5fb9b7f3d883d4ddc
cywd <lichunyang@outlook.com>
2018-11-14 13:48:01 +0800

    fix bug

--- CYSuperDemo/CYSuperDemo/Classes/FrameworkCode/Other/Accounts/AccountsViewController.m
+++ CYSuperDemo/CYSuperDemo/Classes/FrameworkCode/Other/Accounts/AccountsViewController.m
@@ -19,17 +19,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    
+    //创建操作对象
+    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+    //通过操作对象 构建社交平台类型示例  这里采用的新浪微博
+    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierSinaWeibo];
+    //进行用户授权请求
+    [accountStore requestAccessToAccountsWithType:accountType options:nil completion:^(BOOL granted, NSError *error) {
+        if (error) {
+            NSLog(@"error = %@", [error localizedDescription]);
+            return;
+        }
+        
+        if (granted) {
+            NSLog(@"授权通过了");
+            
+            
+            NSArray *accounts = [accountStore accountsWithAccountType:accountType];
+            for (ACAccount *account in accounts) {
+                NSLog(@"%@", account.accountDescription);
+            }
+        };
+    }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
