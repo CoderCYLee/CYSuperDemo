80c148dc420d238381efc628fb56e666c6dbf0a5
cywd <lichunyang@outlook.com>
2018-12-11 18:15:32 +0800

    logo kaunch

---
+++ CYSuperDemo/CYSuperWatchDemo Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  CYSuperWatchDemo Extension
+//
+//  Created by cyrill on 2018/12/11.
+//  Copyright © 2018 Cyrill. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ CYSuperDemo/CYSuperWatchDemo Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  CYSuperWatchDemo Extension
+//
+//  Created by cyrill on 2018/12/11.
+//  Copyright © 2018 Cyrill. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
