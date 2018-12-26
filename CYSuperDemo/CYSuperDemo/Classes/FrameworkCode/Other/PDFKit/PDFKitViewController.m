70a9ca6740451267772738940b40089200b48205
cywd <lichunyang@outlook.com>
2018-12-26 10:56:44 +0800

    cc

--- CYSuperDemo/CYSuperDemo/Classes/FrameworkCode/Other/PDFKit/PDFKitViewController.m
+++ CYSuperDemo/CYSuperDemo/Classes/FrameworkCode/Other/PDFKit/PDFKitViewController.m
@@ -8,8 +8,12 @@
 
 #import "PDFKitViewController.h"
 #import <PDFKit/PDFKit.h>
+#import <Masonry.h>
 
+API_AVAILABLE(ios(11.0))
 @interface PDFKitViewController ()
+
+@property (nonatomic, strong) PDFView *pdfView;
 
 @end
 
@@ -18,8 +22,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    if (@available(iOS 11.0, *)) {
+        
+        [self.view addSubview:self.pdfView];
+        [self.pdfView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self.view);
+        }];
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"pdf1.pdf" ofType:nil];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        PDFDocument *document = [[PDFDocument alloc] initWithURL:url];
+        if (!document) {
+            return;
+        }
+        
+        self.pdfView.document = document;
+        self.pdfView.autoScales = YES;
+        /*
+         kPDFDisplaySinglePage = 0, 单页显示，滚动只影响当前页
+         kPDFDisplaySinglePageContinuous = 1, 显示全部页面，默认垂直排列
+         kPDFDisplayTwoUp = 2,
+         kPDFDisplayTwoUpContinuous = 3
+         */
+        self.pdfView.displayMode = kPDFDisplaySinglePage;
+        self.pdfView.backgroundColor = [UIColor lightGrayColor];
+        
+    } else {
+        // Fallback on earlier versions
+        return;
+    }
+    
+    
+    
 }
 
+
+
+- (PDFView *)pdfView
+API_AVAILABLE(ios(11.0)){
+    if (!_pdfView) {
+        _pdfView = [[PDFView alloc] init];
+    }
+    return _pdfView;
+}
 
 
 @end
